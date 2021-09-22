37e0a37c2d8b1eb22be790f62be553aa64fe04ba
chanoh-park <cocosh6@naver.com>
2021-09-23 01:18:26 +0900

    데이터 불러오기 및 로딩 이미지 추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Input from './components/input';
 import IconButton from './components/IconButton';
 import { images } from './images';
 import Task from './components/Task';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import AppLoading from 'expo-app-loading';
 
 const Container = styled.SafeAreaView`
   flex: 1;
@@ -31,13 +33,23 @@ export default function App() {
   // const width = useWindowDimensions().width;
   const width = Dimensions.get('window').width;
 
+  const [isReady, setIsReady] = useState(false);
   const [newTask, setNewTask] = useState('');
-  const [tasks, setTasks] = useState({
-    1: { id: '1', text: 'Hanbit', completed: false },
-    2: { id: '2', text: 'React Native', completed: true },
-    3: { id: '3', text: 'React Native Sample', completed: false },
-    4: { id: '4', text: 'Edit TODO Item', completed: false },
-  });
+  const [tasks, setTasks] = useState({});
+
+  const _saveTasks = async tasks => {
+    try {
+      await AsyncStorage.setItem('tasks', JSON.stringify(tasks));
+      setTasks(tasks);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const _loadTasks = async () => {
+    const loadedTasks = await AsyncStorage.getItem('tasks');
+    setTasks(JSON.parse(loadedTasks || '{}'));
+  };
 
   const _addTask = () => {
     const ID = Date.now().toString();
@@ -45,25 +57,25 @@ export default function App() {
       [ID]: { id: ID, text: newTask, completed: false },
     };
     setNewTask('');
-    setTasks({ ...tasks, ...newTaskObject });
+    _saveTasks({ ...tasks, ...newTaskObject });
   };
 
   const _deleteTask = id => {
     const currentTasks = Object.assign({}, tasks);
     delete currentTasks[id];
-    setTasks(currentTasks);
+    _saveTasks(currentTasks);
   };
 
   const _toggleTask = id => {
     const currentTasks = Object.assign({}, tasks);
     currentTasks[id]['completed'] = !currentTasks[id]['completed'];
-    setTasks(currentTasks);
+    _saveTasks(currentTasks);
   };
 
   const _updateTask = item => {
     const currentTasks = Object.assign({}, tasks);
     currentTasks[item.id] = item;
-    setTasks(currentTasks);
+    _saveTasks(currentTasks);
   };
 
   const _onBlur = () => {
@@ -74,7 +86,7 @@ export default function App() {
     setNewTask(text);
   };
 
-  return (
+  return isReady ? (
     <ThemeProvider theme={theme}>
       <Container>
         <StatusBar
@@ -105,5 +117,11 @@ export default function App() {
         </List>
       </Container>
     </ThemeProvider>
+  ) : (
+    <AppLoading
+      startAsyn={_loadTasks}
+      onFinish={() => setIsReady(true)}
+      onError={console.error}
+    />
   );
 }
